d1da31d6e50e5037d0ed76a76b7c3d4d188e72b6
Jason Stenhouse <jstenhouse@newrelic.com>
2013-11-06 16:08:45 -0800

    updating plugin versions to latest releases

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,7 +16,7 @@
 #
 class newrelic_plugins::params {
 
-  $aws_cloudwatch_version = '3.1.0'
+  $aws_cloudwatch_version = '3.2.0'
   $aws_cloudwatch_download_baseurl = 'https://github.com/newrelic-platform/newrelic_aws_cloudwatch_plugin/archive'
 
   # nokogiri dependencies
@@ -32,7 +32,7 @@ class newrelic_plugins::params {
   $example_version = '1.0.1'
   $example_download_baseurl = 'https://github.com/newrelic-platform/newrelic_example_plugin/archive/release'
 
-  $mysql_version = '1.0.7'
+  $mysql_version = '1.0.9'
   $mysql_download_baseurl = 'https://raw.github.com/newrelic-platform/newrelic_mysql_java_plugin/master/dist/newrelic_mysql_plugin'
 }
 
