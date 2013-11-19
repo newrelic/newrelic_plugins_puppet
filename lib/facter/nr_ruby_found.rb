c217756b050e34f9843c74a6cb303af6d8358468
Sean P. Kane <skane@newrelic.com>
2013-11-19 14:15:54 -0800

    Support Puppet's embedded ruby and improve the which command usage

--- lib/facter/nr_ruby_found.rb
+++ lib/facter/nr_ruby_found.rb
@@ -1,9 +1,13 @@
 Facter.add(:nr_ruby_found) do
   confine :kernel => :linux
   setcode do
-    ruby = Facter::Util::Resolution.exec('which ruby')
+    if File.exists?('/opt/puppet/bin/ruby')
+      ruby = '/opt/puppet/bin/ruby'
+    else
+      ruby = Facter::Util::Resolution.exec('which --skip-alias ruby 2> /dev/null')
+    end
     if ruby
-      version = Facter::Util::Resolution.exec("ruby -e 'puts RUBY_VERSION'")
+      version = Facter::Util::Resolution.exec( ruby + " -e 'puts RUBY_VERSION'")
       version.start_with?('1.8.7', '1.9', '2.0')
     else
       false
