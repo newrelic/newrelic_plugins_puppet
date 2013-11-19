f12e7c8cdf362f257fe36323bee1fcde0529aed0
Sean P. Kane <skane@newrelic.com>
2013-11-19 14:58:55 -0800

    Correct variable name and improve binary detection

--- lib/facter/nr_java_found.rb
+++ lib/facter/nr_java_found.rb
@@ -1,9 +1,9 @@
 Facter.add(:nr_java_found) do
   confine :kernel => :linux
   setcode do
-    ruby = Facter::Util::Resolution.exec('which java 2>/dev/null')
-    if ruby
-      long_version = Facter::Util::Resolution.exec('/usr/bin/java -version 2>&1')
+    java = Facter::Util::Resolution.exec('which --skip-alias java 2> /dev/null')
+    if java
+      long_version = Facter::Util::Resolution.exec(java + ' -version 2>&1')
       version = long_version.split("\n")[0].split('"')[1]
       version.start_with?('1.6', '1.7')
     else
