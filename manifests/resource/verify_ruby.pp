becf3d6787184cfef5be1f1fa2bd07e80271aabc
Jason Stenhouse <jstenhouse@newrelic.com>
2013-11-22 10:51:57 -0800

    adding urls for java and ruby requirement

--- manifests/resource/verify_ruby.pp
+++ manifests/resource/verify_ruby.pp
@@ -1,5 +1,5 @@
 define newrelic_plugins::resource::verify_ruby {
   unless str2bool($nr_ruby_found) {
-    fail("The New Relic ${name} requires a Ruby version >= 1.8.7 - For more information, see <INSERT_URL_HERE>")
+    fail("The New Relic ${name} requires a Ruby version >= 1.8.7 - For more information, see https://docs.newrelic.com/docs/plugins/ruby-plugin-installation-example")
   }
 }
