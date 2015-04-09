945ec2905fc95fa5b3223d25a27dc160c69e6ce1
malnick <malnick@gmail..com>
2015-04-08 18:53:03 -0700

    service?

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -14,6 +14,28 @@ class newrelic_plugins::haproxy (
     require   => Package['make'],
   }
 
+  package { 'newrelic_plugin':
+    ensure    => present,
+    provider  => 'gem',
+    require   => Package['make'],
+  }
 
+  package { 'bundler':
+    ensure    => present,
+    provider  => 'gem',
+    require   => Package['make'],
+  }
+
+  exec {'newrelic_haproxy_agent_config':
+    command => '/usr/local/bin/newrelic_haproxy_agent install',
+    require => Package['bundler','newrelic_plugin','newrelic_haproxy_agent'],
+    create  => '/etc/newrelic/newrelic_haproxy_agent.yml',
+  }
+
+  service { 'newrelic_haproxy_agent':
+    ensure    => running,
+    start     => '/usr/local/bin/newrelic_haproxy_agent run',
+    subscribe => Exec['newrelic_haproxy_agent_config'],
+  }
 
 }
