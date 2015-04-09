9937a7402526778a48be0d2e5263c7825bdbdeb8
malnick <malnick@gmail..com>
2015-04-09 09:05:56 -0700

    base install option

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -17,13 +17,13 @@ class newrelic_plugins::haproxy (
   package { 'newrelic_haproxy_agent':
     ensure    => present,
     provider  => 'gem',
-    require   => Package['make'],
+    require   => Package['make','bundler'],
   }
 
   package { 'newrelic_plugin':
     ensure    => present,
     provider  => 'gem',
-    require   => Package['make'],
+    require   => Package['make','bundler'],
   }
 
   package { 'bundler':
@@ -41,11 +41,23 @@ class newrelic_plugins::haproxy (
     content => template('newrelic_plugins/haproxy/newrelic_haproxy_plugin.erb'),
   }
 
-  # Configure the yaml file
-  file { '/etc/newrelic/newrelic_haproxy_agent.yml':
-    ensure    => file,
-    mode      => '0644',
-    content   => template('newrelic_plugins/haproxy/newrelic_haproxy_agent.yml.erb'),
+  if $base_install {
+    # If base install then install the standard yml, no config
+    exec {'base_install':
+      command => '/usr/local/bin/newrelic_haproxy_agent run',
+      creates => '/etc/newrelic/newrelic_haproxy_agent.yml',
+      require => Package['newrelic_haproxy_agent','newrelic_plugin'],
+    }
+  }
+
+  else {  
+    # Configure the yaml file - requires that haproxy_agents hash is not empty
+    file { '/etc/newrelic/newrelic_haproxy_agent.yml':
+      ensure    => file,
+      mode      => '0644',
+      content   => template('newrelic_plugins/haproxy/newrelic_haproxy_agent.yml.erb'),
+      require => Package['newrelic_haproxy_agent','newrelic_plugin'],
+    }
   }
 
   service { 'newrelic_haproxy_plugin':
@@ -53,5 +65,4 @@ class newrelic_plugins::haproxy (
     enable    => true,
     subscribe => File['newrelic_haproxy_agent_init','/etc/newrelic/newrelic_haproxy_agent.yml'], 
   }
-
 }
