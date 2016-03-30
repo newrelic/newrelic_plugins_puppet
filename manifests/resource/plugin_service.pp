9db0718ea10dcf8a334f4654d3919fb745663f63
Gabriel Baker <me@g403.co>
2016-03-30 12:27:33 +0100

    Add systemd service for ubuntu > 14.10

--- manifests/resource/plugin_service.pp
+++ manifests/resource/plugin_service.pp
@@ -10,15 +10,27 @@ define newrelic_plugins::resource::plugin_service (
   $service_ensure = running,
   $daemon         = '',
 ) {
-  file { "/etc/init.d/${name}":
+
+  case $::operatingsystem {
+    'Ubuntu': {
+      if versioncmp($::operatingsystemmajrelease, '14.10') > 0 {
+        $config_file = "/etc/systemd/system/${name}.service"
+        $template    = 'newrelic_plugins/systemd_service.erb'
+      } else {
+        $config_file = "/etc/init.d/${name}"
+        $template    = 'newrelic_plugins/service.erb'
+      }
+    }
+  }
+  file { $config_file:
     ensure  => $ensure,
-    content => template('newrelic_plugins/service.erb'),
+    content => template($template),
     mode    => '0755',
   }
 
   service { $name:
     ensure    => $service_ensure,
     enable    => $service_enable,
-    subscribe => File["/etc/init.d/${name}"]
+    subscribe => File[$config_file]
   }
 }
